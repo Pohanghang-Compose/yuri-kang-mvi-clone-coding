40671a456edc1beb2fc44e7a7ee53dd55440fe45
kangyuri1114 <yuri3334@gmail.com>
2024-02-27 21:55:39 +0900

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "mvi-clone-coding"
+include(":app")
+ 
