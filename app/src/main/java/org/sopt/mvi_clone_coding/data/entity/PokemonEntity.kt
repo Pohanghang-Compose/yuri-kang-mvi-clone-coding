15497dffc6633d6731efe588b17818ce201f93f6
kangyuri1114 <yuri3334@gmail.com>
2024-03-03 20:32:00 +0900

    [add] Entity 추가

---
+++ app/src/main/java/org/sopt/mvi_clone_coding/data/entity/PokemonEntity.kt
@@ -0,0 +1,22 @@
+package org.sopt.mvi_clone_coding.data.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(
+    tableName = "pokemons",
+)
+data class PokemonEntity(
+    @PrimaryKey(autoGenerate = false)
+    val id: Int,
+    val avgSpawns: Double,
+    val candy: String,
+    val candyCount: Int,
+    val egg: String,
+    val height: String,
+    val name: String,
+    val num: String,
+    val spawnChance: Double,
+    val spawnTime: String,
+    val weight: String,
+)
---
+++ app/src/main/java/org/sopt/mvi_clone_coding/data/entity/TypeEntity.kt
@@ -0,0 +1,47 @@
+package org.sopt.mvi_clone_coding.data.entity
+
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
+import org.sopt.mvi_clone_coding.data.enum.PokemonType
+
+@Entity(
+    tableName = "types",
+    foreignKeys = [
+        ForeignKey(
+            entity = PokemonEntity::class,
+            parentColumns = arrayOf("id"),
+            childColumns = arrayOf("pokemonId"),
+            onDelete = ForeignKey.CASCADE,
+        ),
+    ],
+)
+class TypeEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    val pokemonId: Int,
+    val value: String,
+) {
+    companion object {
+        fun TypeEntity.toPokemonType(): PokemonType {
+            return when (value) {
+                "Grass" -> PokemonType.GRASS
+                "Poison" -> PokemonType.POISON
+                "Fire" -> PokemonType.FIRE
+                "Flying" -> PokemonType.FLYING
+                "Water" -> PokemonType.WATER
+                "Bug" -> PokemonType.BUG
+                "Normal" -> PokemonType.NORMAL
+                "Electric" -> PokemonType.ELECTRIC
+                "Ground" -> PokemonType.GROUND
+                "Fighting" -> PokemonType.FIGHTING
+                "Psychic" -> PokemonType.PSYCHIC
+                "Rock" -> PokemonType.ROCK
+                "Ice" -> PokemonType.ICE
+                "Ghost" -> PokemonType.GHOST
+                "Dragon" -> PokemonType.DRAGON
+                else -> PokemonType.UNKNOWN
+            }
+        }
+    }
+}
